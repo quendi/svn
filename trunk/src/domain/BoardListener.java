e3fe0e7b3dcb5a3d252baefdebcebb9f26a580fe
samareh.shahmohammadi@gmail.com <samareh.shahmohammadi@gmail.com@79a2487c-e11b-7851-3b13-752d6a6ad440>
2012-04-16 15:47:37 +0000

    Worked on UI some more

--- trunk/src/domain/BoardListener.java
+++ trunk/src/domain/BoardListener.java
@@ -1,10 +1,9 @@
 package domain;
 
-/**
- * User: Joe
- * Date: Apr 14, 2012
- * Time: 5:59:12 PM
- */
-public interface BoardListener {
-    public void placeTile(Tile t);
+
+
+public class BoardListener extends GUI{
+    public void placeTile(Tile t) {
+    	
+	}
 }
