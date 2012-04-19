7b0203462265942474c3d33378fd3737b527afc4
Quendi.JJ <Quendi.JJ>
2012-04-19 17:39:52 +0000

    Change some functions to boolean. Create some functions for board shifting, but still working on it.

--- src/domain/BoardListener.java
+++ src/domain/BoardListener.java
@@ -8,6 +8,9 @@ public interface BoardListener {
 	public static final long serialVersionUID = 1L;
 
 	public void placedTile(Tile t);
+	
+	// while shift, remove the icon of the tile which has been set to null
+	public void removedTile(Tile t);
 
     public void placedKnight(Tile t, int numKnights, Color playerColor);
 }
