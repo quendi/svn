3fa3f701d24b68f44e821174be601cb4e3b1622d
Quendi.JJ <Quendi.JJ>
2012-04-20 08:21:22 +0000

    1. For board shifting, I add a new line, because for example while we have 4 player and in one dimension has been placed 9 tiles, the player still can choose right or left side to place the tile. 2. on the board while each dimension's tile has reach its bound it will disable the extra line 3. the graph icon will be resize when there has different number of player

--- src/domain/BoardListener.java
+++ src/domain/BoardListener.java
@@ -6,6 +6,6 @@ import java.awt.*;
 public interface BoardListener {
 	public void placedTile(Tile t);
 	// while shift, remove the icon of the tile which has been set to null
-	public void removedTile();
+	public void removedTile(boolean x_end, boolean y_end);
     public void placedKnight(Tile t, int numKnights, Color playerColor);
 }
