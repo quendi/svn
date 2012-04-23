878869a08c3fd8a8b386734c73e97e95e2246356
jfrancsc <Quendi.JJ>
2012-04-23 21:46:01 +0000

    joe - further changes on knight implementation(incomplete.

--- src/domain/BoardListener.java
+++ src/domain/BoardListener.java
@@ -5,6 +5,7 @@ import java.awt.*;
 
 public interface BoardListener {
 	public void placedTile(Tile t);
+    public void placedCastle(Tile castle);
 	// while shift, remove the icon of the tile which has been set to null
 	public void removedTile(boolean x_end, boolean y_end);
     public void placedKnight(Tile t, int numKnights, Color playerColor);
