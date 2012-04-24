e484dd7224069704bfe8990f90ba5ebdf7f0a05e
aproffitt29@gmail.com <aproffitt29@gmail.com@79a2487c-e11b-7851-3b13-752d6a6ad440>
2012-04-24 15:50:52 +0000

    aaron - added about screen, need content

---
+++ trunk/src/ui/About.java
@@ -0,0 +1,32 @@
+package ui;
+import javax.swing.*;
+
+public class About {
+	
+	JFrame frame = new JFrame("About");
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
+	
+	public About(){
+		
+		frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        frame.setBounds(750, 300, 500, 500);
+        frame.setResizable(false);
+
+        
+        label.setText("Created by...");
+        frame.add(label);
+        
+	}
+	
+	/***Testing***/
+	public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new About();
+            }
+        });
+	}
+	/******/
+}
