21ee0f7e1420988683d46d2aee5942050d06c7da
samareh.shahmohammadi@gmail.com <samareh.shahmohammadi@gmail.com@79a2487c-e11b-7851-3b13-752d6a6ad440>
2012-04-25 00:49:26 +0000

    Save and load are implemented - not tested.

--- trunk/src/domain/GameState.java
+++ trunk/src/domain/GameState.java
@@ -4,14 +4,7 @@ import java.io.Serializable;
 
 // To be used for saving/loading of game.
 
-/**
-
- * User: Joe
- * Date: Apr 10, 2012
- * Time: 6:17:21 PM
-
- */
 @SuppressWarnings("serial")
 public class GameState implements Serializable {
-    
+	public RobberKnight game;
 }
