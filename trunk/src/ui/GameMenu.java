62e4e409e5ad564ed928173944fd156265cd2fbc
jfrancsc@gmail.com <jfrancsc@gmail.com@79a2487c-e11b-7851-3b13-752d6a6ad440>
2012-05-07 02:49:02 +0000

    joe - fixed background music loop

--- trunk/src/ui/GameMenu.java
+++ trunk/src/ui/GameMenu.java
@@ -13,55 +13,58 @@ import domain.RobberKnight;
 
 @SuppressWarnings("serial")
 public class GameMenu extends JMenuBar{
-	/**
-	 * Main bar
-	 */
-	JMenu file = new JMenu("File");
-	JMenu help = new JMenu("Help");
-	/**
-	 * Items under File
-	 */
-	JMenuItem newGame = new JMenuItem("New Game");
-	JMenuItem surrender = new JMenuItem("Surrender");
-	JMenuItem close = new JMenuItem("Quit");
-	/**
-	 * Load and Save
-	 */
-	SaveAndLoad saveAndLoad;
-	/**
-	 * Items under Help
-	 */
-	JMenuItem about = new JMenuItem("About");
-	JMenuItem rules = new JMenuItem("Rules");
-	
-	
-	public GameMenu(boolean inGame, final InitialScreen initialScreen, final RobberKnight game, final GUI gui){
+    /**
+     * Main bar
+     */
+    JMenu file = new JMenu("File");
+    JMenu help = new JMenu("Help");
+    /**
+     * Items under File
+     */
+    JMenuItem newGame = new JMenuItem("New Game");
+    JMenuItem surrender = new JMenuItem("Surrender");
+    JMenuItem close = new JMenuItem("Quit");
+    /**
+     * Load and Save
+     */
+    SaveAndLoad saveAndLoad;
+    /**
+     * Items under Help
+     */
+    JMenuItem about = new JMenuItem("About");
+    JMenuItem rules = new JMenuItem("Rules");
 
 
-		/**
-		 * Add actionlisteners
-		 */
+    public GameMenu(boolean inGame, final InitialScreen initialScreen, final RobberKnight game, final GUI gui){
+
+
+        /**
+         * Add actionlisteners
+         */
         newGame.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-				if(initialScreen != null)
-					initialScreen.dispose();
-				if(gui!= null && gui.InGame != null)
-					gui.InGame.dispose();
-				new PlayerSelection();
+                if(initialScreen != null)
+                    initialScreen.dispose();
+                if(gui!= null && gui.InGame != null){
+                    gui.InGame.dispose();
+                    gui.stopSound();
+                }
+
+                new PlayerSelection();
             }
         });
         surrender.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-               game.surrender();
+                game.surrender();
             }
         });
         close.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-            	if(gui != null){
-	            	DateFormat df = new SimpleDateFormat("MM-dd-yyyy_HH:mm");
-	        		SaveAndLoad.saveGame(df.format(new Date()));
-	        		SaveAndLoad.writeName("LoadNames.txt", df.format(new Date()));
-            	}
+                if(gui != null){
+                    DateFormat df = new SimpleDateFormat("MM-dd-yyyy_HH:mm");
+                    SaveAndLoad.saveGame(df.format(new Date()));
+                    SaveAndLoad.writeName("LoadNames.txt", df.format(new Date()));
+                }
                 System.exit(0);
             }
         });
@@ -75,36 +78,36 @@ public class GameMenu extends JMenuBar{
                 File file = new File("resources/RobberKnights.pdf");
 
                 try {
-        			Desktop.getDesktop().open(file);
-        		} catch (IOException e) {
-        			e.printStackTrace();
-        		}
+                    Desktop.getDesktop().open(file);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
         saveAndLoad = new SaveAndLoad(game, gui, initialScreen, inGame);
-        
-		/**
-		 * Add items to file menu
-		 */
-		file.add(newGame);
-		file.add(surrender);
-		file.add(close);
-		/**
-		 * Add items to help menu
-		 */
-		help.add(about);
-		help.add(rules);
-		/**
-		 * Add items to Bar
-		 */
-		this.add(file);
-		this.add(saveAndLoad);
-		this.add(help);
-		
-		if(!inGame){
-			surrender.setEnabled(false);
-		}
-		
-	}
+
+        /**
+         * Add items to file menu
+         */
+        file.add(newGame);
+        file.add(surrender);
+        file.add(close);
+        /**
+         * Add items to help menu
+         */
+        help.add(about);
+        help.add(rules);
+        /**
+         * Add items to Bar
+         */
+        this.add(file);
+        this.add(saveAndLoad);
+        this.add(help);
+
+        if(!inGame){
+            surrender.setEnabled(false);
+        }
+
+    }
 
 }
