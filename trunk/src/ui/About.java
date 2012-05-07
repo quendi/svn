da9d13178aaa54107a2e0f1d447ee591c1c66682
jfrancsc@gmail.com <jfrancsc@gmail.com@79a2487c-e11b-7851-3b13-752d6a6ad440>
2012-05-07 04:37:49 +0000

    joe - added about

--- trunk/src/ui/About.java
+++ trunk/src/ui/About.java
@@ -1,25 +1,39 @@
 package ui;
+import java.awt.Font;
+
 import javax.swing.*;
 
-public class  About {
-	
-	JFrame frame = new JFrame("About");
+@SuppressWarnings("serial")
+public class  About extends JFrame{
+
 	JPanel panel = new JPanel();
 	JLabel label = new JLabel();
-	
-	public About(){
-		
-		frame.setVisible(true);
-        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        frame.setBounds(750, 300, 500, 500);
-        frame.setResizable(false);
 
-        
-        label.setText("Created by...");
-        frame.add(label);
-        
+	public About(){
+
+		this.setTitle("About");
+		this.setVisible(true);
+        this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        this.setBounds(750, 300, 425, 500);
+        //this.setResizable(false);
+
+        label.setFont(new Font("Serif", Font.PLAIN, 34));
+
+        label.setText("<html> <center>Robber Knights <br> " +
+        		 "Class Project<br>" +
+        		 "CS 536 Software Engineering<br>" +
+        		 "Spring 2012<br>" +
+        		 "Group Members:<br>"+
+        		 "Joel Francisco<br>"+
+        		 "Chun-Chieh Lee (Jim)<br>"+
+        		 "Aaron Proffitt<br>"+
+        		 "Samareh Shahmohammadi<br>"+
+        		 "Yasodhara Siddineni</center></html>");
+
+        this.add(label);
+
 	}
-	
+
 	/***Testing***/
 	public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
